6d976442bc1032ee75209d6fd82ee0920dd5f261
stoneboat <tood2111@gmail.com>
2022-05-17 16:50:23 +0900

    서버쪽 환경변수 설정

--- server/config/production.js
+++ server/config/production.js
@@ -1,6 +1,5 @@
 module.exports = {
-  mongoURI:
-    "mongodb+srv://tood21:abcd12341!@boilerplate.zqy0o.mongodb.net/blog?retryWrites=true&w=majority",
-  access_key: "nIiUHqhlAVmRIp9mTWb7",
-  secret_key: "hgTg6RXwPR0a8JLddj6BKm1GlVC1NEPKdblKlAlq",
+  mongoURI: process.env.MONGO_URI,
+  access_key: process.env.S3_KEY,
+  secret_key: process.env.S3_SECRET,
 };
